104ed2ec48a27bfcd1805802f6e3b1d73f16197a
Leandro Rodrigues <leandro.l2r@gmail.com>
2021-10-20 18:27:18 -0300

    Revert "Enable shm of selenium default options"
    This reverts commit 4f90891e65bf0b47472994d3464ab1d63e2858ec.

--- promobot/monitor.py
+++ promobot/monitor.py
@@ -93,6 +93,7 @@ class Monitor():
 
         self.options.add_argument('--headless')
         self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--disable-dev-shm-usage')
         self.options.add_argument('--disable-gpu')
         self.options.add_argument('--safe-mode')
         self.options.add_argument('--disable-setuid-sandbox')
